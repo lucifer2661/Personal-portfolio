19940df043fbc70361cc6c433e9268a281ff0955
lucifer2661 <aditya.hackerface@gmail.com>
2026-08-14 03:28:42 +0530

    Fix CLI stylesheet import

--- src/Components/Windows/Cli.jsx
+++ src/Components/Windows/Cli.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react'
 import MacWindow from './MacWindow'
-import './cli.scss'
+import './Cli.scss'
 
 const Cli = ({ onClose }) => {
   const lastLogin = new Date().toString().replace(/GMT.*/, '').trim()
